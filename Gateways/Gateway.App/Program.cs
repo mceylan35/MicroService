72aea697a805427ba0ad45f309f1e0e741134221
mehmet.ceylan <mehmetceylan2696@gmail.com>
2023-11-16 22:48:54 +0300

    add token handler

--- Gateways/Gateway.App/Program.cs
+++ Gateways/Gateway.App/Program.cs
@@ -1,18 +1,20 @@
+using Gateway.App.DeleteHandlers;
 using Ocelot.DependencyInjection;
-using Ocelot.Middleware;
-using Ocelot.Values;
+using Ocelot.Middleware; 
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOcelot();
 var services = builder.Services;
-//services.AddHttpClient<TokenExhangeDelegateHandler>();
-//services.AddAuthentication().AddJwtBearer("GatewayAuthenticationScheme", options =>
-//{
-//    options.Authority = Configuration["IdentityServerURL"];
-//    options.Audience = "resource_gateway";
-//    options.RequireHttpsMetadata = false;
-//});
+services.AddHttpClient<TokenExhangeDelegateHandler>();
+services.AddAuthentication().AddJwtBearer("GatewayAuthenticationScheme", options =>
+{
+    options.Authority = builder.Configuration["IdentityServerURL"];
+    options.Audience = "resource_gateway";
+    options.RequireHttpsMetadata = false;
+});
+
+services.AddOcelot().AddDelegatingHandler<TokenExhangeDelegateHandler>();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
